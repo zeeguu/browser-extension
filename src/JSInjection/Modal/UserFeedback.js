4e3406400d8c2625c4e59c0394e8e0ecc462e708
Darinka <dariinkagm@gmail.com>
2023-11-19 19:51:43 +0100

    Success message after sending feedback

--- src/JSInjection/Modal/UserFeedback.js
+++ src/JSInjection/Modal/UserFeedback.js
@@ -6,79 +6,98 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import SendIcon from '@mui/icons-material/Send';
-import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
+import Alert from '@mui/material/Alert';
 import colors from "../colors";
 
-export default function UserFeedback({ api, articleId, url }) {
-  const [feedback, setFeedback] = useState("");
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [isEmpty, setIsEmpty] = useState(false);
+const UserFeedback = ({ api, articleId, url }) => {
+  const initialFeedbackState = "";
+  const [feedback, setFeedback] = useState(initialFeedbackState);
+  const [isFeedbackSent, setIsFeedbackSent] = useState(false);
 
   const setModalIsOpenToTrue = () => {
-    if (feedback === "" || feedback.length>0 && feedback.replace(/\s/g, '').length==0 || feedback.length==0) {
-      setIsEmpty(true);
-    } else {
-      setIsEmpty(false);
-      setModalIsOpen(true);
+    if (!feedback.trim()) {
+      return;
     }
+    setIsFeedbackSent(true);
+    sendFeedback();
   };
 
-  const setModalIsOpenToFalse = () => {
-    setModalIsOpen(false);
+  const handleChange = (e) => {
+    setFeedback(e.target.value);
+    setIsFeedbackSent(false);
   };
 
-  function handleChange(e) {
-    setFeedback(e.target.value);
-    if (feedback !== "") {
-      setIsEmpty(false);
-    }
-  }
+  const sendFeedback = async () => {
+    await sendFeedbackEmail(api, feedback, url, articleId, "PROBLEM_");
+    resetInput();
+  };
 
-  function submitFeedback(e) {
-    e.preventDefault();
-    if (!isEmpty) {
-      sendFeedbackEmail(api, feedback, url, articleId, "PROBLEM_")
-      resetInput(e);
-    }
-  }
-
-  function resetInput(e){
-    e.target.value = "";
-    setFeedback(e.target.value);
-  }
+  const resetInput = () => {
+    setFeedback(initialFeedbackState);
+  };
 
   return (
-      <Accordion style={{ boxShadow: "none"}}>
-        <AccordionSummary
-          sx={{ justifyContent:'space-evenly', backgroundColor: `${colors.lighterBlue}`, color: `${colors.black}`, borderBottom: ".5px solid white", borderTopLeftRadius: '0.2rem', borderTopRightRadius: '0.2rem'}}
-          expandIcon={<ExpandMoreIcon sx={{ color: `${colors.black}`, fontSize: '0.9rem' }}/>}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography sx={{ fontWeight: '200', fontSize: '0.9rem' }}>Report problems</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <form fullWidth style={{ display: 'flex', alignItems: 'center', padding: '0.5rem'}} onSubmit={submitFeedback}>
-                  <TextField id="outlined-multiline-flexible"
-                  label="Feedback"
-                  multiline
-                  maxRows={3} 
-                  value={feedback} 
-                  onChange={handleChange}
-                  defaultValue="Small"
-                  margin="normal"
-                  size="small" 
-                  error={isEmpty}/>
-                  <div>
-                    <IconButton  type="submit" value="Send feedback" onClick={setModalIsOpenToTrue} id="feedback-box" aria-label="send"> 
-                      <SendIcon sx={{ color: `${colors.darkBlue}`, fontSize:"medium" }}/>
-                    </IconButton>
-                  </div>
-          </form>
-        </AccordionDetails>
-      </Accordion> 
+    <Accordion style={{ boxShadow: "none" }}>
+      <AccordionSummary
+        sx={{
+          justifyContent: 'space-evenly',
+          backgroundColor: colors.lighterBlue,
+          color: colors.black,
+          borderBottom: ".5px solid white",
+          borderTopLeftRadius: '0.2rem',
+          borderTopRightRadius: '0.2rem'
+        }}
+        expandIcon={<SendIcon sx={{ color: colors.black, fontSize: '0.9rem' }} />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography sx={{ fontWeight: '200', fontSize: '0.9rem' }}>Report problems</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        {isFeedbackSent ? (
+          <Grid container justifyContent="center" alignItems="center">
+            <Grid item xs={12}>
+              <Alert severity="success">Feedback sent</Alert>
+            </Grid>
+          </Grid>
+        ) : (
+          <Grid container alignItems="center" spacing={2}>
+            <Grid item xs={12} sm={9}>
+              <TextField
+                id="outlined-multiline-flexible"
+                label="Feedback"
+                multiline
+                maxRows={3}
+                value={feedback}
+                onChange={handleChange}
+                margin="normal"
+                size="small"
+                error={!feedback.trim()}
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <IconButton
+                type="submit"
+                onClick={setModalIsOpenToTrue}
+                id="feedback-box"
+                aria-label="send"
+                disabled={!feedback.trim()}
+                sx={{
+                  color: !feedback.trim() ? colors.gray : colors.darkBlue,
+                  fontSize: "medium",
+                }}
+              >
+                <SendIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        )}
+      </AccordionDetails>
+    </Accordion>
   );
-}
+};
+
+export default UserFeedback;
