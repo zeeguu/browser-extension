c92a679863874799adee61be2b65e97e360d7928
Darinka <dariinkagm@gmail.com>
2023-11-26 23:04:02 +0100

    temporary fix for feedback bug

--- src/popup/PopupContent.js
+++ src/popup/PopupContent.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { setCurrentURL } from "./functions";
 import sendFeedbackEmail from "../JSInjection/Modal/sendFeedbackEmail";
 import { runningInChromeDesktop } from "../zeeguu-react/src/utils/misc/browserDetection";
-import Alert from '@mui/material/Alert';
-import WarningOutlinedIcon from '@mui/icons-material/WarningOutlined';
 
 export default function PopupContent({
   isReadable,
@@ -13,59 +11,47 @@ export default function PopupContent({
   api,
   sessionId,
 }) {
-  const [feedbackSent, setFeedbackSent] = useState(false);
-  const [feedbackSuccess, setFeedbackSuccess] = useState(false);
-  const LANGUAGE_FEEDBACK = "I want this language to be supported";
-  const READABILITY_FEEDBACK = "I think this article should be readable";
+  const LANGUAGE_FEEDBACK = "This language is not supported yet";
+  const READABILITY_FEEDBACK = "This text is not readable";
 
-  useEffect(() => {
-    if (isReadable && languageSupported) {
-      openModal();
+  async function openModal() {
+    if (runningInChromeDesktop()) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["./main.js"],
+        func: setCurrentURL(tab.url),
+      });
+    } else {
+      browser.tabs.executeScript(
+        tab.id,
+        { file: "./main.js" },
+        setCurrentURL(tab.url)
+      );
     }
-  }, [isReadable, languageSupported]);
+    window.close();
+  }
 
-  const openModal = async () => {
-    const executeScriptOptions = {
-      target: { tabId: tab.id },
-      files: ["./main.js"],
-      func: setCurrentURL(tab.url),
-    };
-
-    try {
-      if (runningInChromeDesktop()) {
-        await chrome.scripting.executeScript(executeScriptOptions);
-      } else {
-        await browser.tabs.executeScript(tab.id, { file: "./main.js" }, setCurrentURL(tab.url));
-      }
-      window.close();
-    } catch (error) {
-      console.error("Error occurred:", error);
-      setModalError(true);
-    }
-  };
-
-  const sendFeedbackHandler = (feedback, feedbackType) => {
+  function sendFeedback(feedback, feedbackType) {
     api.session = sessionId;
     sendFeedbackEmail(api, feedback, tab.url, undefined, feedbackType);
-    setFeedbackSent(true);
-    setFeedbackSuccess(true);
-  };
+  }
 
-  const renderFeedbackSection = (feedback, feedbackType, buttonLabel) => (
+  const renderFeedbackSection = (feedback, feedbackType) => (
     <>
-          {<h1>Oh no!</h1>}
-          {<p>{feedback}</p>}{<br/>}
-          {!feedbackSent && sendFeedbackHandler(feedback, feedbackType)}
+      {<h1>Oh no!</h1>}
+      {<p>{feedback}</p>}
+      {sendFeedback(feedback, feedbackType)}
     </>
   );
 
   if (!isReadable) {
-    return renderFeedbackSection("This text is not readable", "READABLE_", "Report page");
+    return renderFeedbackSection(READABILITY_FEEDBACK, "READABLE_");
+  } else{
+    if (!languageSupported) {
+      return renderFeedbackSection(LANGUAGE_FEEDBACK, "LANGUAGE_");
+    }
+    if (languageSupported) {
+      return<>{openModal()}</>;
+    }
   }
-
-  if (isReadable && !languageSupported) {
-    return renderFeedbackSection("This language is not supported yet", "LANGUAGE_", "Do you want us to support this language?");
-  }
-
-  return <></>;
 }
