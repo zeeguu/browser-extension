6eb0ed68c897b3a29fdabf946f8603e520ad9999
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-08-07 15:10:18 +0200

    Reverted dev changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const WEB_URL = "https://www.zeeguu.org";
-const WEB_LOGIN_URL = "https://www.zeeguu.org/login";
+const WEB_URL = "https://zeeguu.org";
+const WEB_LOGIN_URL = "https://zeeguu.org/login";
 const API_URL = "https://api.zeeguu.org";
 
 export { WEB_URL, API_URL, WEB_LOGIN_URL };
