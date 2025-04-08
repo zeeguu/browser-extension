cf4e6e498843f131d5d05385b6d258ef7f730412
emilHojgaard <emilhn94@gmail.com>
2025-04-08 14:33:35 +0200

    started refactoring

--- src/RSWordExtractor/ContentScript.js
+++ src/RSWordExtractor/ContentScript.js
@@ -1,3 +1,4 @@
+import createSmallOverlay from "./smallOverlay.js";
 //Debugging  check
 console.log("Content script loaded");
 
@@ -11,62 +12,14 @@ document.addEventListener("mouseup", (event) => {
     if (isOverlayActive) return;
 
     let selection = window.getSelection();
-    selectedText = selection.toString();
     let url = window.location.href;
     let documentTitle = document.title;
-    // Debugging log:
-    console.log("documentTitle: " + documentTitle);
+
+    selectedText = selection.toString();
 
     if (selectedText.length > 0) {
-        showSmallOverlay(event.clientX, event.clientY, selectedText, url, documentTitle);
+        createSmallOverlay(event.clientX, event.clientY, isOverlayActive, selectedText, url, documentTitle);
         selection.removeAllRanges();
     }
 });
 
-function showSmallOverlay(x, y, selectedText, url, documentTitle) {
-    // Remove any existing small overlay
-    const existingSmallOverlay = document.getElementById("smallOverlay");
-    if (existingSmallOverlay) existingSmallOverlay.remove();
-
-    //Create a new small overlay
-    const smallOverlay = document.createElement("img");
-    smallOverlay.id = "smallOverlay";
-    smallOverlay.style.position = "absolute";
-    smallOverlay.style.top = `${y + window.scrollY}px`;
-    smallOverlay.style.left = `${x + window.scrollX}px`;
-    smallOverlay.style.background = "rgba(255, 255, 255, 0.5)";
-    smallOverlay.style.padding = "5px 10px";
-    smallOverlay.style.borderRadius = "10px";
-    smallOverlay.style.border = "1px solid orange";
-    smallOverlay.style.cursor = "pointer";
-    smallOverlay.style.zIndex = "1000";
-    smallOverlay.src = chrome.runtime.getURL("/images/selection/gold-elephant16.png");
-
-    document.body.appendChild(smallOverlay);
-    isOverlayActive = true;
-
-    // Saved in variable to be able to clear it onClick:
-    let timeoutId = setTimeout(() => {
-        if (smallOverlay) {
-            smallOverlay.remove();
-            isOverlayActive = false;
-            // Debugging log:
-            console.log("isoverlayactive(timeout) " + isOverlayActive);
-        }
-    }, 3000);
-
-    smallOverlay.addEventListener("click", () => {
-
-        // Send selected text, URL and documentTitle to the background script
-        chrome.runtime.sendMessage({
-            type: "SELECTED_TEXT",
-            selectedText: selectedText,
-            url: url,
-            documentTitle: documentTitle
-        });
-
-        smallOverlay.remove();
-        clearTimeout(timeoutId); // Clear the timeout to prevent it from running
-        isOverlayActive = false;
-    });
-}
